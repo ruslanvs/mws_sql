f467b69440950ed49e5495c03b086c52875fab3c
ruslanvs <ruslanvs@yahoo.com>
2018-04-20 19:53:31 -0700

    Data seed and basic fetching of schools

---
+++ db.sql
@@ -0,0 +1,45 @@
+;
+\c test;
+DROP DATABASE IF EXISTS mws;
+CREATE DATABASE mws;
+
+\c mws;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE FUNCTION updated_at() RETURNS trigger AS $some_name$
+    BEGIN
+        NEW.updated_at := CURRENT_TIMESTAMP;
+        RETURN NEW;
+    END;
+$some_name$ LANGUAGE plpgsql;
+
+CREATE TABLE schools(
+    title VARCHAR ( 100 ) NOT NULL,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TRIGGER updated_at BEFORE UPDATE ON cards
+FOR EACH ROW EXECUTE PROCEDURE updated_at();
+
+CREATE TABLE students(
+    name VARCHAR ( 100 ) NOT NULL,
+    score DECIMAL,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TRIGGER updated_at BEFORE UPDATE ON cards
+FOR EACH ROW EXECUTE PROCEDURE updated_at();
+
+-- ******** SEED DATA ********
+INSERT INTO schools( title )
+VALUES
+    ( 'Hogwarts' ),
+    ( 'Beauxbatons' ),
+    ( 'Castelobruxo' ), 
+    ( 'Durmstrang Institute' ),
+    ( 'Ilvermorny' );
+
+SELECT * FROM schools;
